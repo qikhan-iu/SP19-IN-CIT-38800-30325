a077115d0963a6680b87dd25cf5d1c17db50e44a
Quamrul Khan <khanq@iupui.edu>
2019-04-22 19:56:33 -0400

    added new examples

--- src/edu/iupui/cit388/json/JsonProcessor.java
+++ src/edu/iupui/cit388/json/JsonProcessor.java
@@ -9,16 +9,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JsonProcessor {
 
 	public static void main(String[] args) {
-		createJsonFromJavaObject();
+		// this is sample 1
+//		createJsonFromJavaObject();
 		
-		createJavaObjectFromJson();
+//		createJavaObjectFromJson();
+		
+//		createJavaObjectFromJsonV2();
+		
+		createJavaObjectFromJsonV3();
 	}
 
 	private static void createJsonFromJavaObject() {
 
 		try {
+			// this allows you to process a json file
 			ObjectMapper objectMapper = new ObjectMapper();
 
+			
 			Item item1 = new Item();
 			item1.setId("101");
 			item1.setName("Egg");
@@ -34,12 +41,22 @@ public class JsonProcessor {
 			item3.setName("Bread");
 			item3.setPrice(1.49);
 
+			Item item4 = new Item();
+			item4.setId("104");
+			item4.setName("Juice");
+			item4.setPrice(4.49);
+
 			Items items = new Items();
+			
 			items.getItems().add(item1);
 			items.getItems().add(item2);
 			items.getItems().add(item3);
+			items.getItems().add(item4);
 
+			
+			// converts the java object to a json string
 			objectMapper.writeValue(new File("./resource/items.json"), items);
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -51,7 +68,7 @@ public class JsonProcessor {
 		try {
 			ObjectMapper objectMapper = new ObjectMapper();
 
-			Items items = objectMapper.readValue(new File("./resource/items.json"), Items.class);  
+			Items items = objectMapper.readValue(new File("./resource/items2.json"), Items.class);  
 
 			for (Item item : items.getItems()) {
 				System.out.println(item);
@@ -67,11 +84,27 @@ public class JsonProcessor {
 		try {
 			ObjectMapper objectMapper = new ObjectMapper();
 
-			List<Item> items = objectMapper.readValue(new File("./resource/items.json"), new TypeReference<List<Item>>(){});  
+			List<Item> items = objectMapper.readValue(new File("./resource/items3.json"), new TypeReference<List<Item>>(){});  
 
 			for (Item item : items) {
 				System.out.println(item);
 			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	private static void createJavaObjectFromJsonV3() {
+
+		try {
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+
+			Item item = objectMapper.readValue(new File("./resource/items4.json"), Item.class);  
+
+			System.out.println(item);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
